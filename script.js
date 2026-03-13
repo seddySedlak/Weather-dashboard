6d6b921cc514395c15be93c265b649f2b23889c9
Tomáš SeddY Sedlák <pan.bramboru@seznam.cz>
2026-03-13 23:28:27 +0100

    search for openweathermap and geocoding api, start with async function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { API_KEY } from 'config.js'
+
 const searchForm = document.getElementById('search-form');
 const cityInput = document.getElementById('cityInput');
 
@@ -6,3 +8,9 @@ searchForm.addEventListener('submit', (event) =>{
     const cityName = cityInput.value;
     console.log("City", cityName)
 })
+
+async function getWeather(city){
+    const openweathermap = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${API_KEY}`;
+    const geocoding = '';
+
+}
