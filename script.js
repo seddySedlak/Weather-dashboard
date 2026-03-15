043619eaaf59f196cdf0d1847848f99cd5814c63
seddySedlak <pan.bramboru@seznam.cz>
2026-03-15 18:33:41 +0100

    ending a getweater function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { API_KEY } from 'config.js'
+import { API_KEY } from './config.js'
 
 const searchForm = document.getElementById('search-form');
 const cityInput = document.getElementById('cityInput');
@@ -6,11 +6,52 @@ const cityInput = document.getElementById('cityInput');
 searchForm.addEventListener('submit', (event) =>{
     event.preventDefault();
     const cityName = cityInput.value;
-    console.log("City", cityName)
+    if (cityName != "") {
+        console.log("City", cityName)
+        getWeather(cityName)
+        cityInput.value = "";
+    }
 })
 
 async function getWeather(city){
-    const openweathermap = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${API_KEY}`;
-    const geocoding = '';
+    try{
+        const geocoding = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+        const geoResponse = await fetch(geocoding);
+    
+        if (!geoResponse.ok) {
+            throw new Error("Problem connecting to the Geocoding API.");
+        }
+    
+        const geoData = await geoResponse.json();
+    
+        if (geoData.length == 0) {
+            throw new Error("We couldn't find the city. Try a different name.");
+        }
+    
+        const lat = geoData[0].lat
+        const lon = geoData[0].lon
+        console.log(`lat: ${lat}, lot: ${lon}`)
+    
 
+        const openweathermap = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=cz`;
+        const owmResponse = await fetch(openweathermap);
+
+        if (!owmResponse.ok) {
+            throw new Error(`Problem downloading data (Error: ${owmResponse.status})`);
+        }
+
+        const weatherData = await owmResponse.json();
+        console.log("DATA:", weatherData);
+        updateDashboard(city, weatherData)
+    }catch (error) {
+        console.error(`Aplication error: ${error.message}`)
+    }
+}
+
+function updateDashboard(cityName, weatherData){
+    const current = weatherData.current;
+    const today = weatherData.daily[0]
+
+    document.getElementById('cityName').innerText = cityName;
+    document.getElementById('mainTemp').innerText = Math.round(current.temp)
 }
